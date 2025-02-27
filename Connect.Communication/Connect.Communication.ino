1433b2f00f178a5a3754a56022882bf41906c5de
Billet Mickael <mbillet@novacor.fr>
2025-02-27 10:30:31 +0100

    Serial Communication

--- Connect.Communication/Connect.Communication.ino
+++ Connect.Communication/Connect.Communication.ino
@@ -1,6 +1,3 @@
-#include <SPI.h>
-#include <WiFiNINA.h>
-
 #include "NewRemoteReceiver.h"
 #include "NewRemoteTransmitter.h"
 #include "System.h"
@@ -9,8 +6,8 @@
 #include "CircularBuffer.h"
 #include "Sensor.h"
 #include "F007th.h"
-#include "WebSocketConnect.h"
 #include "SerializerJson.h"
+#include "SoftwareSerial.h"
 
 static const int SENSORS_COUNT = 10;
 
@@ -18,27 +15,21 @@ static const short NOT_STARTED = 0;
 static const short STARTING = 1;
 static const short STARTED = 2;
 
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = SECRET_SSID;    // your network SSID (name)
-char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
-
 int status = NOT_STARTED;
 System ArduinoSystem;
+SoftwareSerial raspberrySerial(2, 3);
 
 Sensor* Sensors[SENSORS_COUNT];
-
-WiFiClient wifi;
-WebSocketClient client = WebSocketClient(wifi, IP_SERVER, PORT);
 
 CircBufferMacro(CirBuffer, 32);
 
 void setup()
 {
   status = STARTING;
-  String ipAddress = "";
   
   // initialize serial communication
-  Serial.begin(9600);    
+  Serial.begin(115200);    
+  raspberrySerial.begin(9600);
 
   // set the LED pin mode
   pinMode(System::RED, OUTPUT);      
@@ -46,80 +37,48 @@ void setup()
   pinMode(System::GREEN, OUTPUT);
 
   NewRemoteReceiver::init(4, 2, ReceivePlugStatus);
-  ipAddress = ArduinoSystem.Launch(ssid, pass);
-  if (ipAddress == "")
-  {
-    ArduinoSystem.ResetArduinoWifiRev2();
-  }
-  else
-  {  
-    ArduinoSystem.Initialize(ipAddress, Sensors);
+  
+  ArduinoSystem.Initialize(Sensors);
     
-    F007th::Get()->Initialize();
-    PrintWifiStatus();    
-  }
+  F007th::Get()->Initialize();
 }
 
 void loop()
 { 
-  WebSocketConnect Socket = WebSocketConnect(client);
-  Socket.Initialize("/ws");
   delay(100);
 
-  while (Socket.Connected())
+  String out_data;  
+
+  if (status == STARTING)
   {
-    String out_data;  
-
-    if (status == STARTING)
-    {
-      ArduinoSystem.SendSystemStatus(Socket, System::SystemStartedOrder);
-      status = STARTED;
-    }
-
-    ArduinoSystem.CheckReboot();
-    F007th::Get()->ReadindProcess();
-
-    //Send the Plug status to the Webserver (Interuption)
-    if(circ_bbuf_pop(&CirBuffer, &out_data) == 0)
-    {
-      ArduinoSystem.SendPlugStatus(Socket, out_data.c_str());
-    }
-  
-    //Read the Sensor data periodically from the sensors and send to the WebServer
-    ArduinoSystem.ReadSensorsData(Socket);
-  
-    //Catch the Sensor event from the sensors and send to the WebServer
-    ArduinoSystem.ReadSensorsEvent(Socket);
-  
-    ArduinoSystem.ReadMessageFromWebServer(Socket);   
+    ArduinoSystem.SendSystemStatus(raspberrySerial, System::SystemStartedOrder);
+    status = STARTED;
   }
-  asm volatile("jmp 0x00");
+
+  ArduinoSystem.CheckReboot();
+  F007th::Get()->ReadindProcess();
+
+  //Send the Plug status to the Webserver (Interuption)
+  if(circ_bbuf_pop(&CirBuffer, &out_data) == 0)
+  {
+    ArduinoSystem.SendPlugStatus(raspberrySerial, out_data.c_str());
+  }
+
+  //Read the Sensor data periodically from the sensors and send to the WebServer
+  ArduinoSystem.ReadSensorsData(raspberrySerial);
+
+  //Catch the Sensor event from the sensors and send to the WebServer
+  ArduinoSystem.ReadSensorsEvent(raspberrySerial);
+
+  ArduinoSystem.ReadFromWebServer(raspberrySerial);   
 }
 
 //Interruption when we receive 433Mhz
 void ReceivePlugStatus(NewRemoteCode receivedCode)
 {
-  String in_data = SerializerJson::SerializePlugStatus(System::PlugStatusMsg, receivedCode);
+  String in_data = Serializer::SzPlugStatus(System::PlugStatusMsg, receivedCode);
   if (in_data.length() > 0)
   {
     circ_bbuf_push(&CirBuffer, in_data);
   }
-}
-
-void PrintWifiStatus() 
-{
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // print your board's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
 }
